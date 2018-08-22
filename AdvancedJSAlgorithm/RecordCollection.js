483ccd43cf80959ff13e8c800d7b1febc070bc1e
klunwebale <klunwebale@gmail.com>
2018-08-22 09:17:52 +0300

    Record Collection

---
+++ AdvancedJSAlgorithm/RecordCollection.js
@@ -0,0 +1,52 @@
+var collection ={
+ 2548:{
+  album:"slippery When Wet",
+  artist :"Bon Jovi",
+  tracks:["Let it Rock" ,
+  "You Give Love a bad Name"
+]
+      
+ },
+
+ 2468:{
+    album:"1999",
+    artist :"Prince",
+    tracks:["1999" ,
+    "Little Red Corvette"
+    ]
+        
+   },
+
+1245:{
+    artist :"Robert Palmer",
+    tracks:[]
+},
+
+5439:{
+    album :"ABBA Gold"
+ }
+};
+
+var collectionCopy  =JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id,prop,value){
+    if (prop ==="tracks" &&  value !==""){
+         if(collection[id][prop]){
+            collection[id][prop].push(value); 
+         }else{
+            collection[id][prop]= [value]
+         }
+
+    }else if(value !==""){
+        collection[id][prop] ==value;        
+    }else{
+         delete collection[id][prop];
+    }
+  return collection;
+}
+
+console.log("Get Satrted");
+console.log(updateRecords(2548,"album","Loose Goose"));
+console.log(updateRecords(2468,"album","Sum 21"));
+console.log(updateRecords(1245,"album","Michael Jackson"));
+console.log(updateRecords(5439,"album","Hotel California"));
